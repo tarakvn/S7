3cf7a8404a5b96e367d469b10701e7a62c2da1ae
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:31:27 +0430

    Add project files.

---
+++ S7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the rectangle: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            int width = int.Parse(Console.ReadLine());
+            int Area = length * width;
+            int peri = (length + width) * 2;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The area of rectangle is " + Area);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("The perimeter of the rectangle is " + peri);
+            Console.ReadKey();
+        }
+    }
+}
